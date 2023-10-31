221d7fac58d33a1a5d7c300154f2e55410ec0f06
Vincentyz22 <2806707338@qq.com>
2023-10-31 13:47:07 +0800

    合并版本1

--- 艾维索测试版本yz.js
+++ 艾维索测试版本yz.js
@@ -1,6 +1,6 @@
 while (true) {
   sleep(5000);
-  //主线程
+  //视频浏览主线程
   var mainThread = threads.start(function () {
     var avisoButton = descContains("AVISO").classNameContains("android.view.View").findOne(100);
     descContains("AVISO").classNameContains("android.view.View").waitFor();
@@ -40,18 +40,55 @@ while (true) {
     }
   });
 
-  for (i = 0; i <= 600; i++) {
+  for (i = 0; i <= 300; i++) {
     sleep(1000)
-    if (i == 600) {
+    if (i == 300) {
       toastLog("已运行" + i / 60 + "分钟")
-      // 1分钟后中断主线程
+      // 5分钟后中断主线程
       mainThread.interrupt();
     }
   }
   sleep(3000);
   //关闭标签
   closeLabel();
+  sleep(5000);
+
+  //跳转至网页浏览
+  app.startActivity({
+    packageName: "com.android.chrome",
+    className: "org.chromium.chrome.browser.ChromeTabbedActivity",
+    data: "https://aviso.bz/work-serf"
+
+  });
   sleep(3000);
+  //网页浏览主线程
+  var mainThreadOfWebPage = threads.start(function () {
+    toastLog("程序开始");
+    var avisoButton = descContains("AVISO").classNameContains("android.view.View").findOne(100);
+    descContains("AVISO").classNameContains("android.view.View").waitFor();
+    // 在初始页面
+    if (avisoButton) {
+      var ЗаработатьButton = idContains("mnu_title1").textContains("Заработать").classNameContains("android.widget.TextView").findOne(100);
+      // 找到Заработать标识，表示在初始页面
+      if (ЗаработатьButton) {
+        text("Серфинг").className("android.widget.TextView").waitFor();
+        var СерфингButton = text("Серфинг").className("android.widget.TextView").findOne(100);
+        if (СерфингButton) {
+          sleep(500);
+          // 网页浏览主方法
+          var count = 0;
+          while (true) {
+            pageMain();
+            count++;
+            if (count == 5) {
+              toastLog("结束");
+              break;
+            }
+          }
+        }
+      }
+    }
+  });
 }
 
 // 主流程
@@ -244,7 +281,7 @@ function resolveVideo() {
 
 // 关闭浏览器标签
 function closeLabel() {
-  sleep(500);
+  sleep(3000);
   app.startActivity({
     packageName: "com.android.chrome",
     className: "org.chromium.chrome.browser.ChromeTabbedActivity",
@@ -307,6 +344,32 @@ function closeLabel() {
           }
         }
       }
+    }
+  }
+}
+
+//网页浏览流程
+function pageMain() {
+  toastLog("准备浏览页面");
+  var returnedList = idContains("start-serf").classNameContains("android.view.View").untilFind();
+  for (var i = 0, len = returnedList.length; i < len; i++) {
+    console.log(i);
+    var flow1 = returnedList[i];
+    click(flow1.bounds().centerX() - 80, flow1.bounds().centerY() + random(-5, 1));
+    sleep(1500);
+    var flow2 = textContains("Приступить").classNameContains("android.widget.TextView").findOnce();
+    sleep(1500);
+    toastLog(flow2);
+    if (flow2) {
+      click(flow2.bounds().centerX() + random(-5, 5), flow2.bounds().centerY() + random(-5, 5));
+      textContains("Подтвердить просмотр").classNameContains("android.widget.TextView").waitFor();
+      var browseOver = textContains("Подтвердить просмотр").classNameContains("android.widget.TextView").findOne(100);
+      sleep(1500);
+      click(browseOver.bounds().centerX() + random(-5, 5), browseOver.bounds().centerY() + random(-5, 5));
+      sleep(1500);
+      back();
+      sleep(1500);
+      back();
     }
   }
 }
